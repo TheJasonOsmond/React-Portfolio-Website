ace1f728e055cab72adb05be9025dcbcd7c566f0
Jason Osmond <jasonosmond2001@gmail.com>
2023-05-03 14:42:01 -0600

    Non-npm

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 
 
 const Header = () => {
@@ -11,8 +14,14 @@ const Header = () => {
         <h1>Jason Osmond</h1>
         <h5 className='text-light'> Fullstack Developer </h5>
         <CTA/>
+        <HeaderSocials/>
       </div>
 
+      <div className='me'>
+        <img src={ME} alt="" />
+      </div>
+
+      <a href='#contact' className='scroll_down'> Scroll Down</a>
     </header>
   )
 }
