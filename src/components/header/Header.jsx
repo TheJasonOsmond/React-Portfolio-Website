27d3cd20f1d2d9acea9fb6ffeb677b956d336a6d
Jason Osmond <jasonosmond2001@gmail.com>
2023-05-03 17:53:03 -0600

    Header Created

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,19 +9,19 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-      <div className=".container.header__container">
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Jason Osmond</h1>
         <h5 className='text-light'> Fullstack Developer </h5>
         <CTA/>
         <HeaderSocials/>
-      </div>
 
-      <div className='me'>
-        <img src={ME} alt="" />
-      </div>
+        <div className='me'>
+          <img src={ME} alt="" />
+        </div>
 
-      <a href='#contact' className='scroll_down'> Scroll Down</a>
+        <a href='#contact' className='scroll_down'> Scroll Down</a>
+      </div>
     </header>
   )
 }
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
+import {BsLinkedin,BsGithub} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
         <a href="https://www.linkedin.com/in/jasonosmond/" target='_blank'><BsLinkedin/></a> 
-        <a href="https://github.com/TheJasonOsmond" target='_blank'></a> 
-
+        <a href="https://github.com/TheJasonOsmond" target='_blank'><BsGithub/></a> 
     </div>
   )
 }
