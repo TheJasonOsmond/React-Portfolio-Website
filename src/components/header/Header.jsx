236164a00255c1791afd9242e98e63319865b29a
Jason Osmond <jasonosmond2001@gmail.com>
2023-05-03 18:00:17 -0600

    Header Finished

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Jason Osmond</h1>
-        <h5 className='text-light'> Fullstack Developer </h5>
+        <h5 className='text-light'> Software Developer </h5>
         <CTA/>
         <HeaderSocials/>
 
@@ -20,7 +20,7 @@ const Header = () => {
           <img src={ME} alt="" />
         </div>
 
-        <a href='#contact' className='scroll_down'> Scroll Down</a>
+        <a href='#contact' className='scroll__down'> Scroll Down</a>
       </div>
     </header>
   )
