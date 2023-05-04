de984070007e13596a2a5cd7f5826d995f33cd07
Jason Osmond <jasonosmond2001@gmail.com>
2023-05-03 19:00:10 -0600

    Navbar Complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5> Get To Know </h5>
+      <h2> About Me </h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'> 
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5> Experience </h5>
+              <small> 0 Years Working </small>
+            </article>
+            
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5> Experience </h5>
+              <small> 0 Years Working </small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5> Experience </h5>
+              <small> 0 Years Working </small>
+            </article>
+          </div>
+        </div>
+      </div>   
+    </section>
+
+
   )
 }
 
