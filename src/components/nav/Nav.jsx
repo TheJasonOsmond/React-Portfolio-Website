7a22d18c253c7161e6cfcfc3c24261fd0b47031e
Jason Osmond <jasonosmond2001@gmail.com>
2023-05-05 15:56:34 -0600

    Portfolio and Testimonials Added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -15,8 +15,8 @@ const Nav = () => {
     <nav>
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}> <AiOutlineHome/> </a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} > <AiOutlineUser/> </a>
-      <a href='#experience'onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <BiBriefcase/> </a>
-      <a href='#services'onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}> <BiBook/> </a>
+      <a href='#experience'onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <BiBook/> </a>
+      <a href='#services'onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}> <BiBriefcase/> </a>
       <a href='#portfolio'onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}> <FaReact/> </a>
       <a href='#contact'onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <BiMessageSquareDetail/> </a>
     </nav>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar1.jpg'
+import AVTR3 from '../../assets/avatar1.jpg'
+import AVTR4 from '../../assets/avatar1.jpg'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5> Reviews from clients </h5>
+      <h2> Testimonials</h2>
+
+      <Swiper 
+          slidesPerView={1}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination, Navigation]}
+          className='container testimonials__container'>
+        <SwiperSlide className='testimonial'>
+          <div className='client__avatar'> 
+            <img src={AVTR1} alt="Avatar One"/>
+          </div>
+          <h5 className='clien__name'>Client Name</h5>
+          <small className='client__review'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Quis inventore praesentium exercitationem iure vero, harum, vel rem, 
+            laborum aliquam doloribus illum similique placeat autem veritatis saepe quas!
+            Possimus, accusamus beatae.
+          </small> 
+        </SwiperSlide>
+        <SwiperSlide className='testimonial'>
+          <div className='client__avatar'> 
+            <img src={AVTR1} alt="Avatar One"/>
+          </div>
+          <h5 className='clien__name'>Client Name</h5>
+          <small className='client__review'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Quis inventore praesentium exercitationem iure vero, harum, vel rem, 
+            laborum aliquam doloribus illum similique placeat autem veritatis saepe quas!
+            Possimus, accusamus beatae.
+          </small> 
+        </SwiperSlide>
+      </Swiper>
+    </section>
   )
 }
 
