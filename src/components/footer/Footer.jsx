6a34f438d10c786d3e316f5efd41f1a00e3e1ba5
Jason Osmond <jasonosmond2001@gmail.com>
2023-05-10 19:51:10 -0600

    Main Elements Completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin,BsGithub} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'> Jason Osmond</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'> Home</a></li>
+        <li><a href='#about'> About</a></li>
+        <li><a href='#experience'> Experience</a></li>
+        <li><a href='#portfolio'> Portfolio</a></li>
+        <li><a href='#contact'> Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://www.linkedin.com/in/jasonosmond/" target='_blank'><BsLinkedin/></a> 
+        <a href="https://github.com/TheJasonOsmond" target='_blank'><BsGithub/></a> 
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; 2023 - Developed by Jason Osmond</small>
+      </div>
+    </footer>
   )
 }
 
